297f5c390c8b651b75ea9468475a7e79f85ea8cd
Massin <massin.elotmani@gmail.com>
2022-10-03 22:06:07 +0200

    beetje stukjes

--- moder.py
+++ moder.py
@@ -1,6 +1,16 @@
-bestand = open("opmerkingen.txt", "a")
-for line in bestand.readlines():
+opmerk = open("opmerkingen.txt", "r")
+goed = open("Goedegekeurt.txt", "a")
+for line in opmerk.readlines():
+    opmerk = open("opmerkingen.txt", "r")
+    goed = open("Goedegekeurt.txt", "a")
     print(line)
-    response= input("Type 'Ja' om goedtekeuren type 'Nee' om aftekeuren ")
+    response = input("Type 'Ja' om goedtekeuren type 'Nee' om aftekeuren ")
     if response == "Ja" or response == "ja" or response == "jA" or response == "JA":
         print("ok")
+        goed.write(line)
+        opmerk = open("opmerkingen.txt", "w")
+        line.strip("\n")
+    else:
+        print("je moeder")
+opmerk.close()
+goed.close()
