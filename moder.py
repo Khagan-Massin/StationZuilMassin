a29375d769219e28e547145cc08170e3f943d78b
Massin <massin.elotmani@gmail.com>
2022-10-04 11:42:26 +0200

    duidelijker voor de database

--- moder.py
+++ moder.py
@@ -4,12 +4,12 @@ for line in opmerk.readlines():
     opmerk = open("opmerkingen.txt", "r")
     goed = open("Goedegekeurt.txt", "a")
     print(line)
-    response = input("Type 'Ja' om goedtekeuren type 'Nee' om aftekeuren ")
+    response = input("Type 'Ja' om goedtekeuren type 'Nee' om afteke uren ")
     if response == "Ja" or response == "ja" or response == "jA" or response == "JA":
         print("ok")
         goed.write(line)
         opmerk = open("opmerkingen.txt", "w")
-        #line.strip("\n")
+        line.strip("\n")
     else:
         print("niet ok")
 opmerk.close()
