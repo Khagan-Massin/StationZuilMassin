468ead46de299548cedd8ad34140b6c21a38096a
Massin <massin.elotmani@gmail.com>
2022-10-13 11:03:37 +0200

    werkt better

--- moder.py
+++ moder.py
@@ -1,22 +1,25 @@
-opmerk = open("opmerkingen.txt", "r")
-goed = open("Goedegekeurt.txt", "a")
-fout = open("niet_goed.txt", "a")
+# Opent alle relevante bestanden
+
+# open het opmerk bestand in read mode waar alle opmerkingen wachten voordat ze woorden beoordeeld
+opmerk = open("Opmerkingen.txt", "r")
+# Schrijft Afgekeurd berichten naar Afgekeurd en goedgekeurde berichten naar goed Goedgekeurd
+goed = open("Goedgekeurd.txt", "a")
+fout = open("Afgekeurd.txt", "a")
+
+#Print (naam, gekozen_station, bericht, tijd) naar het scherm.
+#Als de gebruiker "ja" typt woord het naar "Goedgekeurt.txt" geschrijven
+#Zo niet word het naar "Afgekeurd.txt" geschrijven
+
 for line in opmerk.readlines():
-    opmerk = open("opmerkingen.txt", "r")
-    goed = open("Goedegekeurt.txt", "a")
-    fout = open("niet_goed.txt", "a")
     print(line)
-    response = input("Type 'Ja' om goedtekeuren type 'Nee' om afteke uren ")
+    response = input("Type 'Ja' om goedtekeuren type 'Nee' om aftekeuren ")
     if response == "Ja" or response == "ja" or response == "jA" or response == "JA":
-        print("ok")
-        line = line.strip("\n")
+        print("Goedgekeurd")
         goed.write(line)
 
     else:
-        print("niet")
-        line = line.strip("\n")
+        print("Afgekeurd")
         fout.write(line)
-
 
 
 opmerk.close()
